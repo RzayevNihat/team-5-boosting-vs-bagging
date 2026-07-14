d0f1c9d9184576ae5086b161c893133e0fada29f
Fidan200508 <allahverdiyeva200508@gmail.com>
2026-07-14 17:23:52 +0400

    Add StandardScaler class

--- src/utils/preprocessing.py
+++ src/utils/preprocessing.py
@@ -3,3 +3,10 @@ Preprocessing utilities for the project.
 """
 
 import numpy as np
+
+class StandardScaler:
+    """Standardize features using mean and standard deviation."""
+
+    def __init__(self):
+        self.mean_ = None
+        self.scale_ = None
